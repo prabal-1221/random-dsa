b45ef9ceeec3e08f1cb2a7a9eaa5339f7205c3de
notOtaku <mauryaprabal46@gmail.com>
2026-01-27 22:25:32 +0530

    Added leetcode-3650 problem

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Useful uv Commands
+| Task               | Command                |
+| ------------------ | ---------------------- |
+| Create project     | `uv init`              |
+| Create virtual env | `uv venv`              |
+| Install dep        | `uv add pkg`           |
+| Install dev dep    | `uv add --dev pkg`     |
+| Remove dep         | `uv remove pkg`        |
+| Run script         | `uv run script.py`     |
+| Lock & sync deps   | `uv sync`              |
+| List installed     | `uv pip list`          |
+| Upgrade dependency | `uv add --upgrade pkg` |
+
+<br>
+<br>
+
+# LeetCode Solutions
+
+## Problems
+- [3650. Minimum Cost Path with Edge Reversals](https://leetcode.com/problems/minimum-cost-path-with-edge-reversals/description/)
---
+++ solutions/3650-minimum-cost-path-with-edge-reversals.py
@@ -0,0 +1,41 @@
+import heapq
+from collections import defaultdict
+from typing import List
+import ast
+
+class Solution:
+    def minCost(self, n: int, edges: List[List[int]]) -> int:
+        graph = defaultdict(list)
+
+        for u, v, w in edges:
+            graph[u].append((v, w))
+            graph[v].append((u, 2 * w))
+
+        dist = [float('inf')] * n
+        dist[0] = 0
+        pq = [(0, 0)]
+
+        while pq:
+            cost, node = heapq.heappop(pq)
+
+            if node == n - 1:
+                return cost
+
+            if cost > dist[node]:
+                continue
+
+            for nei, wt in graph[node]:
+                new_cost = cost + wt
+                if new_cost < dist[nei]:
+                    dist[nei] = new_cost
+                    heapq.heappush(pq, (new_cost, nei))
+
+        return -1
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    edges = ast.literal_eval(input().strip())
+
+    sol = Solution()
+    print(sol.minCost(n, edges))
